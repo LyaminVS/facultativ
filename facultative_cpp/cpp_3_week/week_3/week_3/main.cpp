75872f67bb2df7799a91b030f7d441d40747b249
LyaminVS <vasya.lyamin03@gmail.com>
2022-10-08 15:55:01 +0300

    files renamed

---
+++ facultative_cpp/cpp_3_week/week_3/week_3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  week_3
+//
+//  Created by Лямин Василий on 08.10.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
